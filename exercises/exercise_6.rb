c4f90de0f969e6db958a25d98d86fc80f134bd59
Dan Amiri-Fear <danielowenfear@gmail.com>
2019-01-16 00:57:14 +0000

    completes exercise 7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,14 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+# @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+
+@store1.employees.create(first_name: "Dan", last_name: "Amiri-Fear", hourly_rate: 100)
+@store1.employees.create(first_name: "Julius", last_name: "Penguin", hourly_rate: 200)
+
+@store2.employees.create(first_name: "John", last_name: "Smith", hourly_rate: 5)
+@store2.employees.create(first_name: "Jane", last_name: "Smith", hourly_rate: 10)
+
+puts @store1.employees
+puts @store2.employees
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,15 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Yo! Name a store:"
+new_store = gets.chomp
+
+users_store = Store.create(name: new_store)
+
+users_store.valid?
+
+error = users_store.errors.messages
+error.each do |key, message|
+  puts "#{key} #{message}"
+end
